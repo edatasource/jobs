ba7de32058d8397af0795bcfb1f44f4df7dfd91c
Moataz Emad Nasr <motaz.emad@gmail.com>
2015-08-07 22:06:03 +0200

    remove main()

--- parser-exercise/app-receipts/src/main/java/com/edatasource/receipts/parser/groupon/GrouponPurchaseParserV2.java
+++ parser-exercise/app-receipts/src/main/java/com/edatasource/receipts/parser/groupon/GrouponPurchaseParserV2.java
@@ -1,13 +1,8 @@
 package com.edatasource.receipts.parser.groupon;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
