bf72131cf25119797e748ea065c4c4f9a028bf16
Brandon D. Glesmann <brandon@edatasource.com>
2015-08-06 20:01:00 -0500

    Added sample parser exercise for upwork project

---
+++ parser-exercise/app-receipts/src/main/java/com/edatasource/receipts/parser/groupon/GrouponPurchaseParserV2.java
@@ -0,0 +1,14 @@
+package com.edatasource.receipts.parser.groupon;
+
+import org.jsoup.nodes.Document;
+
+import com.edatasource.receipts.model.ecommerce.EcommerceReceipt;
+
+public class GrouponPurchaseParserV2{
+
+	public static EcommerceReceipt parse(Document doc)  {
+		return null;
+	}
+
+
+}
