5bd84f5ac12263cc51517b2742b9975417fab866
Matt Brunkhorst <brunkhorstmj@lopers.unk.edu>
2017-11-28 20:24:22 -0600

    EDS Coding Interview

--- java-engineer-1/src/CountingThreading.java
+++ java-engineer-1/src/CountingThreading.java
@@ -2,12 +2,36 @@
  * Write a java code that takes input of a number N, and outputs counting from 1 to N in "N" seconds. Use Threads.
  * The user should be able to pass the number "N" to the application as a parameter.
  */
-public class CountingThreading {
+import java.util.Scanner;
 
+public class CountingThreading implements Runnable {
+	
+		
+	
+	public void run()
+	{
+		
+	}
+	
+	
 	public static void main(String[] args) throws Exception{
 		
 		System.out.println("Hello from CountingThreading.java");
-
+		
+		System.out.print("Enter a number:");
+		Scanner s = new Scanner(System.in);
+		int numOfThreads = s.nextInt();
+		
+		for(int i = 0; i < numOfThreads; i++)
+		{
+			CountingThreading c = new CountingThreading();
+			Thread newT  = new Thread(c);
+			newT.start();
+			Thread.sleep(1000);
+			System.out.println(i+1);
+		}
+		
 	}
+	
 
 }
--- java-engineer-1/src/FizzBuzz.java
+++ java-engineer-1/src/FizzBuzz.java
@@ -9,6 +9,26 @@ public class FizzBuzz {
 		
 		System.out.println("Hello from FizzBuzz.java");
 
+		for(int i = 1; i < 101; i ++)
+		{
+			if(i % 3 == 0 && i % 5 == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if(i % 3 == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else if(i % 5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
+
 	}
 
 }
